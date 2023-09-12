28fa12d7b768f358409cf3b1d5c5b29c1d0bf3f0
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-09-12 18:47:14 +0530

    Array max

---
+++ src/main/java/com/bootcoding/junit/array/ArrayMax.java
@@ -0,0 +1,13 @@
+package com.bootcoding.junit.array;
+
+public class ArrayMax {
+    public int getMax(int number[]) {
+        int i;
+        int max = number[0];
+        for (i = 1; i < number.length; i++)
+            if (number[i] > max)
+                max = number[i];
+
+        return max;
+    }
+}
