789a8359f5707ab4e96bb25a8d074c319cdc7163
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-09-12 18:47:27 +0530

    Array even

---
+++ src/main/java/com/bootcoding/junit/array/ArrayEven.java
@@ -0,0 +1,17 @@
+package com.bootcoding.junit.array;
+
+public class ArrayEven {
+public int[] getEven(int[] num){
+    int evenCount =0;
+    for(int n:num){
+        if(n%2==0){
+            evenCount++;
+        }
+    }
+    int[] evenArray = new  int[evenCount];
+    int i=0;
+    for (int n:evenCount){
+
+    }
+}
+}
