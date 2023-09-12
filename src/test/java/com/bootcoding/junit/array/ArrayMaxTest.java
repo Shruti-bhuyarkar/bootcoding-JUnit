2d305cb8ae262fccbe94533306f1b6c0821bf626
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-09-12 18:46:18 +0530

    Array max test

---
+++ src/test/java/com/bootcoding/junit/array/ArrayMaxTest.java
@@ -0,0 +1,17 @@
+package com.bootcoding.junit.array;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayMaxTest {
+@Test
+    public void test(){
+    ArrayMax arrayMax = new ArrayMax();
+        int[] num = {20,30,50,18,23};
+        int actualNum = arrayMax.getMax(num);
+        int expectedNum = 50;
+        assertEquals(expectedNum,actualNum);
+
+    }
+}
